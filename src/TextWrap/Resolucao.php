1a5fe9eb5d4d5cf93d0ab0ad29d1966ce24d4d4a
Gabriel de Freitas Garcia <gabrielfreitasgarcia@globo.com>
2020-11-12 01:11:48 -0300

    Erro encontrado

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -4,64 +4,64 @@ namespace Galoa\ExerciciosPhp\TextWrap;
 
 /**
  * Classe para resolução do desafio.
- *
  */
 
-class Resolucao implements TextWrapInterface {
+class Resolucao implements TextWrapInterface
+{
 
   /**
    * Atributo que recebe o texto a ser trabalhado.
-   * 
+   *
    * @var string
    */
   private $myText;
   /**
    * Atributo que recebe o vetor de caracteres.
-   * 
+   *
    * @var array
    */
   private $uSafe;
   /**
    * Atributo que recebe a posição atual do vetor.
-   * 
+   *
    * @var int
    */
   private $index;
   /**
    * Tamanho total do texto a ser trabalhado.
-   * 
+   *
    * @var int
    */
   private $len;
   /**
    * Contadora para verificar se ultrapassamos o tamanho da linha.
-   * 
+   *
    * @var int
    */
   private $counter;
   /**
    * Atributo que recebe espaço em branco.
-   * 
+   *
    * @var string
    */
-  private $blank; 
-  
+  private $blank;
+
   /**
-  *   {@inheritdoc}
-  */
+   *   {@inheritdoc}
+   */
 
   /**
    * Construtor sem parâmetros que inicializa o objeto com valores default.
    */
 
-  public function __construct() {
+  public function __construct()
+  {
     $this->myText = "";
     $this->uSafe = array();
     $this->index = 0;
     $this->len = 0;
     $this->counter = 0;
     $this->blank = "";
-
   }
 
   /**
@@ -77,16 +77,16 @@ class Resolucao implements TextWrapInterface {
    *   ou se a String for maior do que um caractere.
    *   caractere.
    */
-
-  private function isBlank(string $char) : bool {
+  private function isBlank(string $char): bool
+  {
     $pattern = "/\s/u";
-    if (mb_strlen($char,"UTF-8") > 1) {
-      echo("Not Char");
+    if (strlen($char) > 1) {
+      echo ("Not Char");
       return false;
-    } else{
-      if(preg_match($pattern,$char) == 1) {
+    } else {
+      if (preg_match($pattern, $char) == 1) {
         return true;
-      } else{
+      } else {
         return false;
       }
     }
@@ -96,47 +96,48 @@ class Resolucao implements TextWrapInterface {
    * Método que pega uma palavra do vetor de caracteres e retorna.
    *
    * Atualiza o índice e uma contadora.
-  */
-
-  private function getWord() : string {
+   */
+  private function getWord(): string
+  {
     $word = "";
-    for (; $this->index < $this->len ; $this->index++,$this->counter++) { 
-      if($this->isBlank($this->uSafe[$this->index])) {
+    for (; $this->index < $this->len; $this->index++, $this->counter++) {
+      if ($this->isBlank($this->uSafe[$this->index])) {
         $this->blank = $this->uSafe[$this->index];
         $this->counter++;
+        $this->index++;
         return $word;
-      }else {
-          $word .= $this->uSafe[$this->index];
-        }
+      } else {
+        $word .= $this->uSafe[$this->index];
+      }
     }
     return $word;
   }
 
   /**
    * Método que pega uma palavra maior que um tamanho e quebra em duas palavras.
-   * 
+   *
    * @param string word
    *   Palavra que será quebrada.
    * @param string word2
    *   Referência para retornar segunda palavra.
    * @param int length
    *   Tamanho máximo que um pedaço pode ter.
-   * 
+   *
    * @return string
    *   Retorna por valor primeiro pedaço da palavra e o segundo por referência.
-  */
-  private function breakWord(string $word, string &$word2, int $length) : string {
-    $auxAr = str_split($word,1);
+   */
+  private function breakWord(string $word, string &$word2, int $length): string
+  {
+    $auxAr = str_split($word, 1);
     $word = "";
     $i = 0;
-    for(; $i < $length; $i++) {
+    for (; $i < $length; $i++) {
       $word .= $auxAr[$i];
     }
-    for (; $i < count($auxAr);$i++) {
+    for (; $i < count($auxAr); $i++) {
       $word2 .= $auxAr;
     }
     return $word;
-
   }
 
   /**
@@ -151,46 +152,45 @@ class Resolucao implements TextWrapInterface {
    *   Um array de strings equivalente ao texto recebido por parâmetro porém
    *   respeitando o comprimento de linha e as regras especificadas acima.
    */
-  public function textWrap(string $text, int $length): array {
+  public function textWrap(string $text, int $length): array
+  {
     $result = array();
     $word = "";
     $aux = "";
     $aux2 = "";
     $this->myText = $text;
-    $this->uSafe = str_split($this->myText,1);
+    $this->uSafe = str_split($this->myText, 1);
     $this->len = count($this->uSafe);
-    while($this->index < $this->len){
+    while ($this->index < $this->len) {
       $aux = $this->getWord();
-      if ($this->counter -1 > $length){
-        if(strlen($aux) < $length) {
+      if ($this->counter - 1 > $length) {
+        if (strlen($aux) < $length) {
           array_push($result, $word);
           $word = $aux;
           $this->counter = 0;
-        }
-        else{
-          do{
-            $aux = $this->breakWord($aux,$aux2,$length);
+        } else {
+          do {
+            $aux = $this->breakWord($aux, $aux2, $length);
             $word .= $aux;
-            array_push($result,$word);
+            array_push($result, $word);
             $word = "";
             $aux = $aux2;
-          }while(strlen($aux2) > $length);
+          } while (strlen($aux2) > $length);
           $aux2 .= $this->blank;
           $word = $aux2;
           $this->counter = mb_strlen($aux2);
           $aux2 = "";
         }
-      } elseif ($this->counter - 1 == $length){
-          $word .= $aux;
-          array_push($result, $word);
-          $this->counter = 0;
-          $word = "";
-      } else{
+      } elseif ($this->counter - 1 == $length) {
+        $word .= $aux;
+        array_push($result, $word);
+        $this->counter = 0;
+        $word = "";
+      } else {
         $aux .= $this->blank;
         $word .= $aux;
       }
     }
     return $result;
   }
-
 }
